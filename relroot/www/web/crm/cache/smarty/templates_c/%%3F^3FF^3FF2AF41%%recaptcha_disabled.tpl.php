274f7182c8677db42f7b09b72b33901a08286461
Carlos Santana <csantanad@gmail.com>
2018-10-30 11:21:21 -0500

    add crm

---
+++ relroot/www/web/crm/cache/smarty/templates_c/%%3F^3FF^3FF2AF41%%recaptcha_disabled.tpl.php
@@ -0,0 +1,21 @@
+<?php /* Smarty version 2.6.31, created on 2018-10-24 11:29:38
+         compiled from /var/www/SuiteCRM-hotfix/include/utils/recaptcha_disabled.tpl */ ?>
+<?php echo '
+<script>
+
+  /**
+   * Login Screen Validation
+   */
+  function validateAndSubmit() {
+      generatepwd();
+    }
+
+  /**
+   * Password reset screen validation
+   */
+  function validateCaptchaAndSubmit() {
+      document.getElementById(\'username_password\').value = document.getElementById(\'new_password\').value;
+      document.getElementById(\'ChangePasswordForm\').submit();
+    }
+</script>
+'; ?>
